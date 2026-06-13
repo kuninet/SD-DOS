406d55be8cca58bbd946579777ecfc71f859c1a3
kuninet <kuninet@gmail.com>
2026-06-13 21:33:39 +0900

    feat: VGM再生サンプルにファイルセレクタとBUSY待ち上限を追加
    - 起動時に現在ディレクトリの*.VGMを一覧表示(STRM_DIRENT 600EH使用)
    - 複数桁の番号+Enterで選択して再生、終わるとメニューへ戻る
    - 0/Enterのみで終了(JP BASIC)
    - YM2203 BUSY待ちの上限を9006H(BUSY_MAX)でPOKE可能に(0=無制限)
      密な小節の詰まり緩和の実機実験用
    - ファイル名のハードコード(FNAME)を廃止
    検証ハーネスにメニュー駆動(KEYWAIT注入)と複数ファイル選択を追加

--- samples/VGMPLAY.asm
+++ samples/VGMPLAY.asm
@@ -27,6 +27,9 @@ READ_SAMP	EQU	20H		;SD読み1バイトの所要(サンプル換算)。POKE &H9004
 WRITE_COST	EQU	23H		;音源書き込み1回の処理時間(サンプル換算)。POKE &H9005
 CR	EQU	0DH		;
 BASIC	EQU	0081H		;BASICへ復帰(モニタGや自動実行どちらでも安全)
+KEYWAIT	EQU	0F75H		;1文字入力待ち A<-コード
+STRM_DIRENT	EQU	600EH		;ディレクトリ列挙(N番目のファイル名)
+BUSY_MAX	EQU	00H		;YM2203 BUSY待ち上限(0=無制限)。POKE &H9006
 RBUF_SIZE	EQU	1000H		;先読みリングバッファのサイズ（4KB。貯金枠）
 
 	ORG	09000H
@@ -35,43 +38,53 @@ RBUF_SIZE	EQU	1000H		;先読みリングバッファのサイズ（4KB。貯金枠）
 WAIT_KV:	DB	WAIT_K		;9003H ウェイト係数。POKE &H9003,n で実機調整可能
 READ_SAMPV:	DB	READ_SAMP	;9004H SD読みコスト補正(POKE可。大きいほど速い)
 WRITE_COSTV:	DB	WRITE_COST	;9005H 書込処理コスト補正(POKE可。大きいほど密小節を詰める)
+BUSY_MAXV:	DB	BUSY_MAX		;9006H BUSY待ち上限(POKE可。0=無制限)
 
 START:
-	LD	(SAVSP),SP		;異常終了時の脱出用にSPを保存する
-	LD	SP,STACK_TOP		;専用スタックへ(SD-DOSの深い呼び出しでBASICスタックを溢れさせない)
-	LD	HL,FNAME		;ファイルを開く
-	CALL	STRM_OPEN		;
-	JR	NC,.OK			;
-	LD	HL,MSG_NF		;見つからなければメッセージを表示して終了
-	CALL	PUTS			;メッセージ表示
-	JP	BASIC			;RETせずBASICへ(G起動でも安全)
-
-.OK:	CALL	RB_INIT			;リングバッファ初期化
-	CALL	PARSE_HDR		;ヘッダを解析してデータ開始位置まで進める
-	CALL	RB_REFILL		;再生開始前にバッファをプライム
-	JP	PLAY			;再生ループへ
+	LD	(SAVSP),SP		;BASIC復帰用にSP保存
+	LD	SP,STACK_TOP		;専用スタックへ
+MENU:
+	CALL	BUILD_LIST		;*.VGM一覧表示、(LISTCNT)=件数
+	LD	A,(LISTCNT)		;0件なら終了
+	OR	A
+	JP	Z,BASIC
+	CALL	READ_NUM		;HL=入力番号(1始まり、0/Enterで終了)
+	LD	A,H			;256以上は無効→再表示
+	OR	A
+	JR	NZ,MENU
+	LD	A,L
+	OR	A
+	JP	Z,BASIC			;0で終了
+	LD	A,(LISTCNT)		;番号>件数なら再表示
+	CP	L
+	JR	C,MENU
+	LD	A,L			;K番目(1始まり)のVGMを選ぶ
+	CALL	GET_NTH_VGM		;PLAYNAMEへ
+	JR	C,MENU
+	CALL	PLAY_FILE		;再生(終わるとここへ戻る)
+	JR	MENU
 
 ;-------------------------------------------------
 ;正常終了（再生終了・EOF）
 ;-------------------------------------------------
 DONE:
-	LD	SP,(SAVSP)		;
-	CALL	STRM_CLOSE		;
-	LD	HL,MSG_END		;
-	CALL	PUTS			;メッセージ表示
-	JP	BASIC			;RETせずBASICへ(G起動でも安全)
+	LD	SP,(PLAYSP)		;再生中の脱出点へ
+	CALL	STRM_CLOSE
+	LD	HL,MSG_END
+	CALL	PUTS
+	RET				;メニューへ戻る
 
 ;-------------------------------------------------
 ;異常終了
 ;IN  HL=メッセージ
 ;-------------------------------------------------
 ABORT:
-	LD	SP,(SAVSP)		;
-	PUSH	HL			;
-	CALL	STRM_CLOSE		;
-	POP	HL			;
-	CALL	PUTS			;メッセージ表示
-	JP	BASIC			;RETせずBASICへ(G起動でも安全)
+	LD	SP,(PLAYSP)		;再生中の脱出点へ
+	PUSH	HL
+	CALL	STRM_CLOSE
+	POP	HL
+	CALL	PUTS
+	RET				;メニューへ戻る
 
 ;-------------------------------------------------
 ;ヘッダ解析（docs/vgm/header.md の最小解析範囲）
@@ -154,47 +167,53 @@ PLAY:
 	LD	A,C			;
 	JP	SKIP_CMD		;その他は読み飛ばし規則へ
 
-.OPN:	CALL	GETB			;C<-レジスタ番号
-	LD	C,A			;
-	CALL	GETB			;B<-データ値
+.OPN:	CALL	GETB			;レジスタ番号
+	LD	(OPNREG),A		;
+	CALL	GETB			;データ値
+	LD	(OPNDAT),A		;
+	LD	A,(BUSY_MAXV)		;BUSY待ち上限(0=無制限)
 	LD	B,A			;
-.BUSY:	IN	A,(OPN_ADDR)		;BUSY（ビット7）が下りるまで待つ
+.BUSY:	IN	A,(OPN_ADDR)		;BUSY(bit7)が下がるまで
 	RLCA				;
-	JR	C,.BUSY			;
-	LD	A,C			;レジスタ番号を出力
+	JR	NC,.BWOK		;下がった
+	LD	A,B			;上限0なら無制限
+	OR	A			;
+	JR	Z,.BUSY			;
+	DJNZ	.BUSY			;上限まで待って打ち切り
+.BWOK:	LD	A,(OPNREG)		;レジスタ番号を出力
 	OUT	(OPN_ADDR),A		;
-	LD	A,(FNAME)		;レジスタ選択後の短い待ち（ダミー読み）
-	LD	A,B			;データ値を出力
+	LD	A,(OPNREG)		;レジスタ選択後の短い待ち(ダミー読み)
+	LD	A,(OPNDAT)		;データ値を出力
 	OUT	(OPN_DATA),A		;
-	CALL	ADD_DEBT			;処理時間debtを加算
-	JR	.LOOP			;
+	CALL	ADD_DEBT		;処理時間debtを加算
+	JP	PLAY.LOOP		;
 
 .PSG:	CALL	GETB			;レジスタ番号を出力
 	OUT	(PSG_ADDR),A		;
 	CALL	GETB			;データ値を出力
 	OUT	(PSG_DATA),A		;
 	CALL	ADD_DEBT			;処理時間debtを加算
-	JR	.LOOP			;
+	JP	PLAY.LOOP			;
 
 .W61:	CALL	GETB			;DE<-サンプル数
 	LD	E,A			;
 	CALL	GETB			;
 	LD	D,A			;
 	CALL	WAIT_DE			;
-	JR	.LOOP			;
+	JP	PLAY.LOOP			;
 .W62:	LD	DE,735			;1/60秒
 	CALL	WAIT_DE			;
-	JR	.LOOP			;
+	JP	PLAY.LOOP			;
 .W63:	LD	DE,882			;1/50秒
 	CALL	WAIT_DE			;
-	JR	.LOOP			;
+	JP	PLAY.LOOP			;
 .W7X:	LD	A,C			;（下位4ビット+1）サンプル
 	AND	0FH			;
 	INC	A			;
 	LD	E,A			;
 	LD	D,0			;
 	CALL	WAIT_DE			;
-	JR	.LOOP			;
+	JP	PLAY.LOOP			;
 
 ;-------------------------------------------------
 ;未対応コマンドの読み飛ばし（docs/vgm/commands.md の規則）
@@ -504,6 +523,183 @@ RB_TRYFILL1:
 ;00H終端文字列の表示
 ;IN  HL=文字列の先頭アドレス
 ;-------------------------------------------------
+;=================================================
+;再生(1ファイル): PLAYNAMEを開いて再生、終わるとRETでメニューへ
+;=================================================
+PLAY_FILE:
+	LD	HL,PLAYNAME
+	CALL	STRM_OPEN
+	JR	C,.nf
+	LD	(PLAYSP),SP		;再生中の脱出点(DONE/ABORTがここへ戻す)
+	CALL	RB_INIT
+	CALL	PARSE_HDR
+	CALL	RB_REFILL
+	JP	PLAY
+.nf:	LD	HL,MSG_NF
+	CALL	PUTS
+	RET
+
+;=================================================
+;*.VGM一覧表示。(LISTCNT)=件数。番号は1始まり
+;=================================================
+BUILD_LIST:
+	LD	HL,MSG_HDR
+	CALL	PUTS
+	XOR	A
+	LD	(LISTCNT),A
+	LD	HL,0
+	LD	(DIRIDX),HL
+.lp:	LD	DE,(DIRIDX)
+	LD	HL,NAMEBUF
+	CALL	STRM_DIRENT
+	JR	C,.done
+	LD	HL,(DIRIDX)
+	INC	HL
+	LD	(DIRIDX),HL
+	CALL	IS_VGM
+	JR	NZ,.lp
+	LD	A,(LISTCNT)
+	INC	A
+	CALL	PRDEC
+	LD	HL,MSG_COLON
+	CALL	PUTS
+	LD	HL,NAMEBUF
+	CALL	PUTS
+	LD	A,CR
+	RST	18H
+	LD	A,(LISTCNT)
+	INC	A
+	LD	(LISTCNT),A
+	JR	.lp
+.done:	LD	HL,MSG_PROMPT
+	CALL	PUTS
+	RET
+
+;=================================================
+;K番目(1始まり)の.VGMをPLAYNAMEへ。CY=0成功/CY=1なし
+;IN A=K
+;=================================================
+GET_NTH_VGM:
+	LD	(TARGETK),A
+	XOR	A
+	LD	(VGMCNT),A
+	LD	HL,0
+	LD	(DIRIDX),HL
+.lp:	LD	DE,(DIRIDX)
+	LD	HL,NAMEBUF
+	CALL	STRM_DIRENT
+	JR	C,.nf
+	LD	HL,(DIRIDX)
+	INC	HL
+	LD	(DIRIDX),HL
+	CALL	IS_VGM
+	JR	NZ,.lp
+	LD	A,(VGMCNT)
+	INC	A
+	LD	(VGMCNT),A
+	LD	HL,TARGETK
+	CP	(HL)
+	JR	NZ,.lp
+	LD	HL,NAMEBUF
+	LD	DE,PLAYNAME
+	LD	BC,0DH
+	LDIR
+	OR	A
+	RET
+.nf:	SCF
+	RET
+
+;=================================================
+;NAMEBUFの拡張子が"VGM"ならZ=1
+;=================================================
+IS_VGM:
+	LD	HL,NAMEBUF
+.f:	LD	A,(HL)
+	OR	A
+	JR	Z,.no
+	CP	2EH
+	JR	Z,.d
+	INC	HL
+	JR	.f
+.d:	INC	HL
+	LD	DE,VGMEXT
+	LD	B,3
+.c:	LD	A,(DE)
+	CP	(HL)
+	JR	NZ,.no
+	INC	HL
+	INC	DE
+	DJNZ	.c
+	LD	A,(HL)
+	OR	A
+	RET
+.no:	OR	0FFH
+	RET
+
+;=================================================
+;10進数入力(数字+Enter)。OUT HL=数値(0=終了)
+;=================================================
+READ_NUM:
+	LD	HL,0
+.k:	PUSH	HL
+	CALL	KEYWAIT
+	POP	HL
+	CP	CR
+	RET	Z
+	CP	30H
+	JR	C,.k
+	CP	3AH
+	JR	NC,.k
+	PUSH	AF
+	RST	18H
+	POP	AF
+	SUB	30H
+	PUSH	DE
+	LD	D,H
+	LD	E,L
+	ADD	HL,HL
+	ADD	HL,HL
+	ADD	HL,DE
+	ADD	HL,HL
+	LD	D,0
+	LD	E,A
+	ADD	HL,DE
+	POP	DE
+	JR	.k
+
+;=================================================
+;A(0-255)を10進表示(先頭ゼロ抑制)
+;=================================================
+PRDEC:
+	LD	E,A
+	LD	D,0
+	LD	B,100
+	CALL	.pl
+	LD	B,10
+	CALL	.pl
+	LD	A,E
+	ADD	A,30H
+	RST	18H
+	RET
+.pl:	LD	C,30H
+.s:	LD	A,E
+	CP	B
+	JR	C,.e
+	SUB	B
+	LD	E,A
+	INC	C
+	JR	.s
+.e:	LD	A,C
+	CP	30H
+	JR	NZ,.pr
+	LD	A,D
+	OR	A
+	RET	Z
+.pr:	LD	A,C
+	RST	18H
+	LD	D,1
+	RET
+
 PUTS:	LD	A,(HL)			;
 	OR	A			;
 	RET	Z			;
@@ -514,6 +710,10 @@ PUTS:	LD	A,(HL)			;
 IDENT:	DB	"Vgm "			;VGM識別子
 TBL9X:	DB	4,4,5,10,1,4		;90H-95Hのオペランド長
 FNAME:	DB	"/MUSIC.VGM",00H	;対象のファイル名（00H終端）
+MSG_HDR:	DB	CR,"-- VGM --",CR,00H
+MSG_COLON:	DB	": ",00H
+MSG_PROMPT:	DB	CR,"NO(0=END)? ",00H
+VGMEXT:	DB	"VGM"
 MSG_NF:	DB	CR,"NOT FOUND",CR,00H	;
 MSG_NOTVGM:
 	DB	CR,"NOT VGM",CR,00H	;
@@ -533,6 +733,15 @@ RB_WRP:		DS	2		;書き込みポインタ
 RB_CNT:		DS	2		;バッファ内バイト数（0～RBUF_SIZE）
 RB_EOF:		DS	1		;先読みが終端に達したら非0
 WDEBT:		DS	2		;処理時間debt(サンプル)
+PLAYSP:		DS	02H			;再生中の脱出点SP
+LISTCNT:	DS	01H			;一覧の件数
+DIRIDX:		DS	02H			;ディレクトリ列挙インデックス
+VGMCNT:		DS	01H			;VGM計数(選択用)
+TARGETK:	DS	01H			;選択された番号(1始まり)
+OPNREG:		DS	01H			;YM2203レジスタ番号一時
+OPNDAT:		DS	01H			;YM2203データ一時
+NAMEBUF:	DS	0DH			;列挙ファイル名("NAME.EXT",0)
+PLAYNAME:	DS	0DH			;選択ファイル名
 STACK:		DS	256		;プレイヤー専用スタック(クラスタ境界の深い呼び出しに対応)
 STACK_TOP	EQU	$		;スタック先頭(SPの初期値。下方向へ伸びる)
 
